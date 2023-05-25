9b7de722b2c427f3980410a928fca22d3938f04d
brahim elkartouch <belkarto@student.1337.ma>
2023-05-25 07:04:46 +0100

    if _else

--- basics.swift
+++ basics.swift
@@ -12,21 +12,22 @@ var myVariable1: Int = 42
 
 //combine a string and an integer using string interpolation
 let label = "The width is "
-let width = 94
+    let width = 94
 let widthLabel = label + String(width)
 //another way to include values in strings is to write the value in parentheses,
 //and write a backslash (\) before the parentheses
+
 let apples = 3
 let oranges = 5
 let appleSummary = "I have \(apples) apples."
 let fruitSummary = "I have \(apples + oranges) pieces of fruit.\n"
 
 let quotation = """
-    I said "\(appleSummary))" and then I said "\(fruitSummary)"
+I said "\(appleSummary))" and then I said "\(fruitSummary)"
 
-        that line is indented in the raw string literal.
-    And then I said "I have \(apples + oranges) pieces of fruit."
-    """
+that line is indented in the raw string literal.
+And then I said "I have \(apples + oranges) pieces of fruit."
+"""
 print(quotation, "\n",widthLabel, "\n", fruitSummary)
 
 //arrays and dictionaries
@@ -42,3 +43,21 @@ var Dict = [
 Dict["watermelon"] = nil
 
 // print(Dict["watermelon"] ?? "no value")
+
+let individualScores = [75, 43, 103, 87, 12]
+var teamScore = 0
+for score in individualScores{
+    if score > 50{
+        teamScore += 3
+    }else{
+        teamScore += 1
+    }
+}
+print(teamScore)
+    for (score) in (individualScores){
+        if (score > 50){
+            teamScore += 3
+        }else{
+            teamScore += 1
+        }
+    }
